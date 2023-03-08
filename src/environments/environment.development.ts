814b4c4007086993caa4c903f96bf40fbba047aa
StayAbroad <dennis.koester11@gmail.com>
2023-03-08 20:11:49 +0100

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-clone-e1e2b',
+    appId: '1:664976476249:web:2d9833015fe0da6b5b8948',
+    storageBucket: 'slack-clone-e1e2b.appspot.com',
+    apiKey: 'AIzaSyBFYQa0mT-pSo4WHM-WTqqCMQTGtVZ21oI',
+    authDomain: 'slack-clone-e1e2b.firebaseapp.com',
+    messagingSenderId: '664976476249',
+  },};
