daeaa4ef7d9412ea4c9bf34496ba027a216fae15
colorfultrees <colorfultrees@users.noreply.github.com>
2023-03-11 19:11:57 +0100

    dialog create channel

--- src/app/components/channels/channels.component.ts
+++ src/app/components/channels/channels.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogCreateChannelComponent } from '../dialog-create-channel/dialog-create-channel.component';
 
 @Component({
   selector: 'app-channels',
@@ -8,8 +10,20 @@ import { Component } from '@angular/core';
 export class ChannelsComponent {
   collapsed = false;
 
+  constructor(public dialog: MatDialog) {}
+
   toggleDropdown() {
     this.collapsed = !this.collapsed;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogCreateChannelComponent);
+
+    dialogRef.afterClosed().subscribe((dialogData) => {
+      if (dialogData.name) {
+        console.log(`Channel '${dialogData.name}' created (private = ${dialogData.isPrivate}).`)
+      }
+    });
   }
 
   createChannel() {
