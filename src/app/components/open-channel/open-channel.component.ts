44ef4f99732084c57e21bffa8d82732f7efa7c53
Daniela Fehlmann <danielafehlmann@gmail.com>
2023-03-13 09:21:46 +0100

    .

--- src/app/components/open-channel/open-channel.component.ts
+++ src/app/components/open-channel/open-channel.component.ts
@@ -19,6 +19,8 @@ export class OpenChannelComponent implements OnInit {
   messages = [];
   channelId = '';
   sendedPostID = '';
+  threadIds: Array<any> = [];
+  blabla = [];
   constructor(private firestore: Firestore, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -29,7 +31,6 @@ export class OpenChannelComponent implements OnInit {
       this.channelId = paramMap.get('id');
     });
     this.loadThreads();
-    console.log('oninit loaded');
   }
 
   loadThreads() {
@@ -42,36 +43,48 @@ export class OpenChannelComponent implements OnInit {
     const order = query(threads, orderBy('timestamp'));
     const lodadId = this.channelId;
     let unsubscribe = onSnapshot(order, async (threads) => {
-      // if (lodadId != this.channelId) {
-      //   unsubscribe();
-      // } else {
+      if (lodadId != this.channelId) {
+        unsubscribe();
+      } else {
       this.startLoading(threads);
-      console.log('threads', threads);
-
-      // }
+      }
     });
   }
 
   startLoading(threads: any) {
+
     const threadsCollection = collection(
       this.firestore,
       'channels',
       this.channelId,
-      'threads'
+      'threads',
+
     );
-    const threads$ = collectionData(threadsCollection, {
-      idField: 'threadId',
-    });
+    const threads$ = collectionData(threadsCollection, {idField: 'threadId',});
     this.messages = [];
-    threads.forEach((messageDoc: any) => {
-      let message = {
-        author: messageDoc.threadId.author,
-        authorImg: messageDoc.authorImg,
-        message: messageDoc.message,
-        timestamp: messageDoc.timestamp,
-      };
-      this.messages.push(message);
-      console.log('id', threads$);
-    });
+    
+    threads$.subscribe((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (!this.threadIds.includes(data[i]['threadId'])) {
+          this.threadIds.push(data[i]['threadId']);
+        }
+      }
+      for (let i = 0; i < this.threadIds.length; i++) {
+        const thread = collectionData(threadsCollection, this.threadIds[i]);
+        console.log(thread);
+      }
+    })
+    
+    // threads.forEach((messageDoc: any) => {
+    //   let message = {
+    //     author: messageDoc.author,
+    //     authorImg: messageDoc.authorImg,
+    //     message: messageDoc.message,
+    //     timestamp: messageDoc.timestamp,
+    //   };
+    //   this.messages.push(message);
+    //   // console.log(this.messages);
+    // });
+    
   }
 }
