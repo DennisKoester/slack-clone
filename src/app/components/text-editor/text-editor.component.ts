92e6a5f2aa201aa998de40425b5d482877d9801c
StayAbroad <dennis.koester11@gmail.com>
2023-03-13 20:53:28 +0100

    create thread and messages

--- src/app/components/text-editor/text-editor.component.ts
+++ src/app/components/text-editor/text-editor.component.ts
@@ -1,42 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { User } from '@firebase/auth';
-import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill/public-api';
+import {
+  EditorChangeContent,
+  EditorChangeSelection,
+} from 'ngx-quill/public-api';
 import { ActivatedRoute } from '@angular/router';
 import { addDoc, collection } from '@firebase/firestore';
-import { collectionData, doc, Firestore, getDoc } from '@angular/fire/firestore';
+import {
+  collectionData,
+  doc,
+  Firestore,
+  getDoc,
+  Timestamp,
+} from '@angular/fire/firestore';
 import { Observable } from '@firebase/util';
 import { DocumentData } from '@angular/fire/compat/firestore';
+import { getAuth } from 'firebase/auth';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 
 @Component({
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.scss'],
 })
-export class TextEditorComponent implements OnInit{
+export class TextEditorComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: Firestore,
+    public authenticationService: AuthenticationService
+  ) {}
 
-  constructor(private route: ActivatedRoute, private firestore: Firestore) {
-    
+  editorText = '';
+  editorAuthor = '';
+  channelId;
+
+  ngOnInit(): void {}
+
+  changedEditor(event: EditorChangeContent | EditorChangeSelection) {
+    this.editorText = event['text'];
+    console.log(this.editorText);
   }
 
-editorText = '' ;
-editorAuthor = '';
-channelId;
+  createThread() {
+    const timestamp = Timestamp.fromDate(new Date());
 
+    this.route.paramMap.subscribe(async (paramMap) => {
+      this.channelId = paramMap.get('id');
+      const threads = collection(
+        this.firestore,
+        'channels',
+        this.channelId,
+        'threads'
+      );
+      const threadId = await addDoc(threads, { timestamp });
+      this.createMessage(threadId, timestamp);
+    });
+  }
 
-ngOnInit(): void {
-}
+  createMessage(threadId: any, timestamp: any) {
+    const currentUserId = JSON.parse(localStorage.getItem('user')).uid;
 
-changedEditor(event: EditorChangeContent | EditorChangeSelection) {
-  this.editorText = event['text'];
-}
-
-toFirebase() {
-  this.route.paramMap.subscribe((paramMap) => {
-    this.channelId = paramMap.get('id');
-    const threads = collection(this.firestore, 'channels', this.channelId, 'threads');
-    addDoc(threads, {});
-  });
-}
-
+    const messages = collection(
+      this.firestore,
+      'channels',
+      this.channelId,
+      'threads',
+      threadId.id,
+      'messages'
+    );
+    addDoc(messages, {
+      author: currentUserId,
+      message: this.editorText,
+      timestamp: timestamp,
+    });
+  }
 }
