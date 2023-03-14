c9590c809654c020009de768accfd9b7a1c3f852
Daniela Fehlmann <danielafehlmann@gmail.com>
2023-03-14 17:48:57 +0100

    .

--- src/app/components/direct-messages-section/direct-messages-section.component.ts
+++ src/app/components/direct-messages-section/direct-messages-section.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection, doc, getDoc } from '@firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-direct-messages-section',
   templateUrl: './direct-messages-section.component.html',
   styleUrls: ['./direct-messages-section.component.scss'],
 })
-export class DirectMessagesSectionComponent {
+export class DirectMessagesSectionComponent implements OnInit{
   collapsed = false;
 
   toggleDropdown() {
     this.collapsed = !this.collapsed;
   }
+constructor(public firestore: Firestore) {
+
 }
+users$: Observable<any>;
+
+ngOnInit() {
+  this.getUsers();
+  
+}
+
+getUsers() {
+  let coll = collection(this.firestore, 'users');
+  this.users$ = collectionData(coll);
+
+  this.users$.subscribe (async () => {
+   
+  })
+}
+  
+
+
+
+  
+}
+
+
