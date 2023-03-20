9465f86c56dd0e50dfc8d3d43ee05bae0b534b02
colorfultrees <colorfultrees@users.noreply.github.com>
2023-03-20 15:28:17 +0100

    chatlist rendering

--- src/app/components/chatlist-item/chatlist-item.component.ts
+++ src/app/components/chatlist-item/chatlist-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chatlist-item',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./chatlist-item.component.scss']
 })
 export class ChatlistItemComponent {
-
+  @Input() name: string;
 }
--- src/app/components/direct-messages-section/direct-messages-section.component.ts
+++ src/app/components/direct-messages-section/direct-messages-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
-import { collection, doc, getDoc } from '@firebase/firestore';
+import { Router } from '@angular/router';
+import { collection, CollectionReference, doc, DocumentData, getDoc } from '@firebase/firestore';
 import { Observable } from 'rxjs';
 import * as GLOBAL_VAR from 'src/app/shared/services/globals';
 import { UsersService } from 'src/app/shared/services/users.service';
@@ -10,33 +11,105 @@ import { UsersService } from 'src/app/shared/services/users.service';
   templateUrl: './direct-messages-section.component.html',
   styleUrls: ['./direct-messages-section.component.scss'],
 })
-export class DirectMessagesSectionComponent implements OnInit {
+export class DirectMessagesSectionComponent {
   collapsed = false;
-  users$: Observable<any>;
+  chatsCollection: CollectionReference;
+  chats$: Observable<DocumentData[]>;
+  chats: Array<any> = [];
+  currentUserId: string;
 
-  constructor(public firestore: Firestore, private usersService: UsersService) {
+  constructor(public firestore: Firestore, private usersService: UsersService, public router: Router) {
     this.usersService.usersCollListener.subscribe({
       next: (users) => null
     });
+    this.currentUserId = JSON.parse(localStorage.getItem('user')).uid;
+    this.getChatsFromDb();
   }
 
   toggleDropdown() {
     this.collapsed = !this.collapsed;
   }
 
-  ngOnInit() {
-    this.getUsers();
 
+  /**
+   * Retrieves all chats from the data base
+   */
+  getChatsFromDb() {
+    this.chatsCollection = collection(this.firestore, GLOBAL_VAR.COLL_CHATS);
+    this.chats$ = collectionData(this.chatsCollection, {idField: 'chatId'});
+    this.chats$.subscribe((chatsData) => {
+      console.log('All chats', chatsData);
+      this.chats = this.filterChats(chatsData);
+    });
   }
 
-  getUsers() {
-    let coll = collection(this.firestore, 'users');
-    this.users$ = collectionData(coll);
 
-    this.users$.subscribe(async () => {
+  /**
+   * Filters all chats of which the logged-in user is part of
+   * @param chatsData The chats retrieved from the data base
+   * @returns Array
+   */
+  filterChats(chatsData: Array<any>) {
+    let chats = chatsData.filter(chat => chat['USERS'].includes(this.currentUserId));
+    console.log(`Privat chats for ${this.currentUserId}`, chats);
+    chats = this.cleanUpUserLists(chats);
+    chats = this.getUserNamesAndImages(chats);
+    return chats;
+  }
 
-    })
+
+  /**
+   * Removes the UID of the logged-in user from the user list of each chat
+   * @param chats The chats of the logged-in user
+   * @returns Array
+   */
+  cleanUpUserLists(chats: Array<any>) {
+    chats.forEach((chat) => {
+      chat['USERS'] = chat['USERS'].filter(user => user != this.currentUserId);
+    });
+    console.log('Filtered chat list: ', chats);
+    return chats;
+  }
+
+
+  /**
+   * 
+   * @param chats The chats of the logged-in user
+   * @returns Array
+   */
+  getUserNamesAndImages(chats: Array<any>) {
+    chats.forEach(chat => {
+      for (let u = 0; u < chat['USERS'].length; u++) {
+        console.log(`Processing chatUser: ${chat['USERS'][u]}`);
+        chat['USERS'][u] = this.getUserMetaData(chat['USERS'][u]);
+        console.log(`After processing chatUser: `, chat['USERS'][u]);
+      }
+
+      // 
+      // TODO: Implement in rendering of chat messages
+      // 
+      // chat['MESSAGES'].forEach(chatMessage => {
+      //   chatMessage['author'] = this.getUserMetaData(chatMessage['author']);
+      // });
+      // 
+      // 
+    });
+    console.log('Metadata implemented: ', chats);
+    return chats;
+  }
+
+
+  /**
+   * Reads 
+   * @param _user The user whose metadata should be read
+   * @returns JSON
+   */
+  getUserMetaData(_user: string) {
+    const userName = this.usersService.usersCollListener.value.users.find(user => _user == user.uid);
+    const userImg = ''; // TODO: UserImg auslesen, wenn implementiert
+    return {
+      'userName': userName.displayName,
+      'userImg': userImg
+    };
   }
 }
-
-
