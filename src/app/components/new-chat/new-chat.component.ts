75173e413b0073658073a5c5c0ca5945d23f9c76
colorfultrees <colorfultrees@users.noreply.github.com>
2023-03-27 17:32:11 +0200

    create new chat

--- src/app/components/new-chat/new-chat.component.ts
+++ src/app/components/new-chat/new-chat.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { addDoc, collection, CollectionReference, Firestore } from '@angular/fire/firestore';
 import { ChatService } from 'src/app/shared/services/chat.service';
 import { UsersService } from 'src/app/shared/services/users.service';
+import * as GLOBAL_VAR from 'src/app/shared/services/globals';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-chat',
@@ -8,11 +11,48 @@ import { UsersService } from 'src/app/shared/services/users.service';
   styleUrls: ['./new-chat.component.scss']
 })
 export class NewChatComponent {
+  selectedUserIds: Array<string> = [];
+  selectedUserNames: Array<string> = [];
+  chatsCollection: CollectionReference;
 
-  constructor(public chatService: ChatService, public usersService: UsersService) {}
+  constructor(
+    public chatService: ChatService,
+    public usersService: UsersService,
+    private firestore: Firestore,
+    private router: Router) { }
 
   resetSearchUser() {
     this.chatService.searchUser = '';
   }
 
+  selectUser(uid: string, name: string) {
+    this.selectedUserIds.push(uid);
+    this.selectedUserNames.push(name);
+  }
+
+  deselectUser(index: number) {
+    this.selectedUserIds.splice(index, 1);
+    this.selectedUserNames.splice(index, 1);
+  }
+
+  async createChat() {
+    this.chatsCollection = collection(this.firestore, GLOBAL_VAR.COLL_CHATS);
+    const newChatId = await this.writeToDatabase();
+    this.router.navigateByUrl(`home/chat/${newChatId}`);
+  }
+
+  async writeToDatabase() {
+    const chatData = {
+      USERS: [this.usersService.currentUserData.uid, ...this.selectedUserIds],
+      MESSAGES: []
+    }
+
+    console.log('ChatData: ', chatData);
+
+    const chatDoc = await addDoc(this.chatsCollection, chatData);
+
+    console.log('New Chat: ', chatDoc.id);
+
+    return chatDoc.id;
+  }
 }
