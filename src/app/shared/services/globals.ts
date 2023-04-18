cdbf1f72524767fb8a3ca6399634fec6b09ff29a
StayAbroad <dennis.koester11@gmail.com>
2023-04-18 22:21:23 +0200

    small fixes and guest user

--- src/app/shared/services/globals.ts
+++ src/app/shared/services/globals.ts
@@ -2,4 +2,4 @@ export const COLL_USERS = 'users';
 export const COLL_CHANNELS = 'CHANNELS';
 export const COLL_THREADS = 'THREADS';
 export const COLL_CHATS = 'CHATS';
-export const guest = 'xfFoKv1etbTxOBDHL9vBlKzAmEG3';
+export const guest = 'UwmeDUlkO5NUh2WdP9bN3Bzcr6v1';
